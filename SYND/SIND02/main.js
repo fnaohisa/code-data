da1cc95332e787252112d09618d572f440485ca3
fnaohisa <71742361+fnaohisa@users.noreply.github.com>
2021-12-21 21:14:18 +0900

    Update main.js

--- SYND/SIND02/main.js
+++ SYND/SIND02/main.js
@@ -1,24 +1,24 @@
 (function(){
-    var bottom_fixed_ad         = document.querySelector(".bottom_fixed_ad"); // 広告のwrapper
-    var gmoam_pr                = document.getElementById("gmoam_pr");  // ゲタ
+    var bottom_fixed_ad = document.querySelector(".bottom_fixed_ad"); // 広告のwrapper
+    var gmoam_pr = document.getElementById("gmoam_pr");  // ゲタ
 
     var ad_move_interval;
     var ad_opacity_interval;
     var ad_timeout;
 
-    const AD_START_HEIGHT       = 150; // 表示位置
-    const AD_HEIGHT_INTERVAL    = 1;
-    const AD_OPACITY_INTERVAL   = 0.1;
-    const TOUCHEND_INTERVAL     = 500;
+    const AD_START_HEIGHT = 150; // 表示位置
+    const AD_HEIGHT_INTERVAL = 1;
+    const AD_OPACITY_INTERVAL = 0.1;
+    const TOUCHEND_INTERVAL = 500;
 
     // ゲタ
     var addGetaDiv = (PR)=>{
-        PR.style.height         = "30px";
-        PR.style.lineHeight     = "30px";
-        PR.style.background     = "#000000";
-        PR.style.textAlign      = "center";
-        PR.style.color          = "#fff";
-        PR.style.fontSize       = "12px";
+        PR.style.height = "30px";
+        PR.style.lineHeight = "30px";
+        PR.style.background = "#000000";
+        PR.style.textAlign = "center";
+        PR.style.color = "#fff";
+        PR.style.fontSize = "12px";
     }
 
     // 最初のdisplay処理
@@ -43,7 +43,7 @@
         // 透過の動き定義
         ad_opacity_interval = setInterval(()=>{
             AD.style.opacity = parseFloat(AD.style.opacity) + parseFloat(AD_OPACITY_INTERVAL);
-            let opacity      = parseFloat(AD.style.opacity);
+            let opacity = parseFloat(AD.style.opacity);
             // opacity 1になったら
             if (opacity >= parseFloat(1)) {
                 AD.style.opacity = 1;
@@ -56,18 +56,18 @@
     // ページ下部にオーバーレイ用のスペース
     var paddingBottom = (ad)=>{
         var padding_div = document.createElement('div');
-        var ad_height   = ad.clientHeight;
+        var ad_height = ad.clientHeight;
         // body タグの下にpaddin_div追加
         padding_div.style.paddingBottom = ad_height + 'px';
-        padding_div.style.background    = 'transparent';
+        padding_div.style.background = 'transparent';
         document.body.appendChild(padding_div);
     }
 
     // reload
     var reload = (AD)=>{
         // 最初非表示
-        AD.style.display        = 'none';
-        gmoam_pr.style.height   = '0px';
+        AD.style.display = 'none';
+        gmoam_pr.style.height = '0px';
         // scrollで連続でdisaply_startを発火しても挙動がおかしくならないように
         clearInterval(ad_move_interval);
         clearInterval(ad_opacity_interval);
