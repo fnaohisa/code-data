32c5e4493b8719b67780f0324735f74489215a55
fnaohisa <71742361+fnaohisa@users.noreply.github.com>
2021-12-21 20:40:46 +0900

    Create index.php

---
+++ sej/index.php
@@ -0,0 +1,462 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>フランチャイズで独立開業ならセブン-イレブン</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+  <meta name="keywords" content="独立開業,起業,加盟,フランチャイズ,オーナー募集,コンビニ経営">
+  <meta name="description" content="【公式】あなたの熱意と私たちセブンイレブンの充実したサポート体制がエリアNo1店を生み出します！コンビニ運営ノウハウを凝縮した手厚いサポートで未経験、初めての独立でも安心。土地・建物がなくてもコンビニ経営者に！充実のサポート体制と保証制度で経営も安心。先輩オーナー本音インタビュー、各エリアオーナー説明会日程情報、相談員の経営アドバイスなど。">
+  <?php $Path = "../"; ?>
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="フランチャイズで独立開業ならセブン-イレブン">
+  <meta property="og:description" content="【公式】あなたの熱意と私たちセブンイレブンの充実したサポート体制がエリアNo1店を生み出します！コンビニ運営ノウハウを凝縮した手厚いサポートで未経験、初めての独立でも安心。土地・建物がなくてもコンビニ経営者に！充実のサポート体制と保証制度で経営も安心。先輩オーナー本音インタビュー、各エリアオーナー説明会日程情報、相談員の経営アドバイスなど。">
+  <meta property="og:image" content="https://www.sej.co.jp/owner/assets/img/common/ogp.png">
+  <meta property="og:url" content="https://www.sej.co.jp/owner/">
+  <meta property="og:site_name" content="フランチャイズで独立開業ならセブン-イレブン">
+  <link rel="icon" href="<?php print $Path; ?>owner/assets/img/common/favicon.ico">
+  <link rel="stylesheet" href="<?php print $Path; ?>owner/assets/css/top.css">
+  <link rel="stylesheet" href="<?php print $Path; ?>owner/assets/css/page-briefing.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/css/swiper.min.css">
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WHSNL3F');</script>
+  <!-- End Google Tag Manager -->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108483242-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-108483242-1', {
+      'linker': {
+        'domains': ['fcowner-hikaku.com', 'sej.co.jp'],
+        'accept_incoming': true
+      }
+    });
+  </script>
+  <?php include("${Path}owner/inc/tag-common-header.php");
+  ?>
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHSNL3F"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <?php include("${Path}owner/inc/header.php"); ?>
+
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <a href="/owner/seminar/webdouga/">
+          <img src="../owner/assets/img/top/fv-bnr-01.png" alt="" class="01 u-for-pc">
+          <img src="../owner/assets/img/top/sp/fv-bnr-01.png" alt="" class="01 u-for-sp">
+        </a>
+      </div>
+      <div class="swiper-slide">
+        <a href="/owner/seminar/">
+          <img src="../owner/assets/img/top/fv-bnr-02.png" alt="" class="02 u-for-pc">
+          <img src="../owner/assets/img/top/sp/fv-bnr-02.png" alt="" class="02 u-for-sp">
+        </a>
+      </div>
+      <div class="swiper-slide">
+        <a href="/owner/area/market/kansai.php">
+          <img src="../owner/assets/img/top/fv-bnr-04.png" alt="" class="04 u-for-pc">
+          <img src="../owner/assets/img/top/sp/fv-bnr-04.png" alt="" class="04 u-for-sp">
+        </a>
+      </div>
+      <div class="swiper-slide">
+        <a href="/owner/ui/">
+          <img src="../owner/assets/img/top/fv-bnr-03.png" alt="" class="03 u-for-pc">
+          <img src="../owner/assets/img/top/sp/fv-bnr-03.png" alt="" class="03 u-for-sp">
+        </a>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-prev swiper-button-white"></div>
+    <div class="swiper-button-next swiper-button-white"></div>
+  </div>
+
+  <section class="slide_title__wrapper">
+    <a href="#intro" class="slide_title">
+      <h2 class="slide_title__heading">
+        セブン-イレブンの強みや<br class="u-for-pc">オーナーの仕事内容などを<br class="u-for-pc">もっと詳しくご紹介！
+      </h2>
+      <div class="slide_title__logo">
+        オーナーの仕事<br>
+        セブン-イレブンの特徴
+      </div>
+      <p>詳しくはクリック</p>
+      <span>&nbsp;</span>
+    </a>
+  </section>
+
+  <div class="business_container">
+
+    <section class="business" id="business">
+      <div class="inner">
+        <h2 class="heading">セブン-イレブンの<br class="u-for-sp">ビジネス</h2>
+        <ul class="business__list">
+          <li>
+            <a href="/owner/business/">
+              <img src="../owner/assets/img/top/business-tmb-01.jpg" alt="セブン-イレブンのフランチャイズビジネス">
+              <h3 class="business__list__title">
+                セブン-イレブンの<br>フランチャイズビジネス
+              </h3>
+            </a>
+          </li>
+          <li>
+            <a href="/owner/actionplan/">
+              <img src="../owner/assets/img/top/business-tmb-02.jpg" alt="セブン-イレブンがいま取り組んでいること">
+              <h3 class="business__list__title">
+                セブン-イレブンが<br>いま取り組んでいること
+              </h3>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="conditions" id="number_01">
+      <div class="inner">
+        <p class="number">1</p>
+        <h2 class="heading">加盟条件について</h2>
+        <ul class="conditions__list">
+          <li>
+            <div class="conditions__list__content">
+              <div class="conditions__list__text">
+                <p class="conditions__list__icon">
+                  <span>A</span>タイプ
+                </p>
+                <h3 class="conditions__list__title">土地・建物を<span>ご自身で用意する</span>タイプ</h3>
+              </div>
+              <p class="conditions__list__tmb">
+                <img src="../owner/assets/img/top/conditions-tmb-01.png" alt="土地・建物をご自身で用意するタイプ">
+              </p>
+            </div>
+            <p class="conditions__list__btn">
+              <a href="/owner/type-a/" class="btn__l_blue">詳しく見る</a>
+            </p>
+          </li>
+          <li>
+            <div class="conditions__list__content">
+              <div class="conditions__list__text">
+                <p class="conditions__list__icon">
+                  <span>C</span>タイプ
+                </p>
+                <h3 class="conditions__list__title">土地・建物を<span>本部で用意する</span>タイプ</h3>
+              </div>
+              <p class="conditions__list__tmb">
+                <img src="../owner/assets/img/top/conditions-tmb-02.png" alt="土地・建物を本部で用意するタイプ">
+              </p>
+            </div>
+            <p class="conditions__list__btn">
+              <a href="/owner/type-c/" class="btn__l_blue">詳しく見る</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="general" id="number_02">
+      <div class="inner">
+        <p class="number">2</p>
+        <h2 class="heading">バックアップ</h2>
+        <ul class="general__list">
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-03.jpg" alt="オーナーを支えるサポート制度">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">オーナーを支える<br>サポート制度
+              </h3>
+              <p class="general__list__text">
+                開業前も、開業後も。セブン-イレブンにはあなたの独立をバックアップする制度が多数あります。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/support/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-04.jpg" alt="移住希望者独立支援制度">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">
+                移住希望者<br>独立支援制度
+              </h3>
+              <p class="general__list__text">
+                実家で独立したい、子供の環境を整えられるエリアで定住したいなど、移住者に対しての支援制度です。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/ui/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="general" id="number_03">
+      <div class="inner">
+        <p class="number">3</p>
+        <h2 class="heading">加盟のプロセス</h2>
+        <ul class="general__list">
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-05.jpg" alt="開店（店舗経営）までの流れ">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">
+                開店（店舗経営）までの流れ
+              </h3>
+              <p class="general__list__text">
+                様々なステップを経て、じっくりとご検討いただき、十分にご理解、納得していただいた上で加盟していただいています。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/flow/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-06.jpg" alt="契約社員からスタート！">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">
+                契約社員からスタート！
+              </h3>
+              <p class="general__list__text">
+                未経験歓迎！転職で契約社員に。「まずはお金を貯めたい」「仕事を経験しておきたい」という方におすすめの制度です。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/agreement/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="general" id="number_04">
+      <div class="inner">
+        <p class="number">4</p>
+        <h2 class="heading">ステップアップ</h2>
+        <ul class="general__list">
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-07.jpg" alt="従業員独立支援制度">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">
+                従業員独立支援制度
+              </h3>
+              <p class="general__list__text">
+                すでに既存店舗で勤務実績のある方の独立を支援する制度です。アルバイトでもパートでも、条件さえ満たせば適用可能。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/independent/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <p class="general__list__tmb">
+              <img src="../owner/assets/img/top/business-tmb-08.jpg" alt="複数店経営奨励制度">
+            </p>
+            <div class="general__list__content">
+              <h3 class="general__list__title">
+                複数店経営奨励制度
+              </h3>
+              <p class="general__list__text">
+                開業後数年経過したオーナー様が新規開店する場合は、セブン-イレブン・チャージを減額する制度です。
+              </p>
+              <p class="general__list__btn">
+                <a href="/owner/encouragement/" class="btn__green_s">詳しく見る</a>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+  </div>
+
+  <?php include("${Path}owner/inc/page-briefing-top.php"); ?>
+
+  <section class="intro" id="intro">
+    <div class="inner">
+      <h2 class="heading">セブン-イレブンの強み・オーナーの仕事を<br class="u-for-pc">もっと詳しくご紹介</h2>
+      <div class="intro__content">
+        <a href="/owner/contents/">
+          <div class="intro__content__tmb">
+            オーナーの仕事<br>セブン-イレブンの特徴
+          </div>
+          <div class="intro__content__text">
+            「セブンオーナーの一日って？」「先輩オーナーのキャリアパスは？」<br>全国各地のオーナーインタビューや商品開発秘話、OFCサポートの実態など、もっと詳しくセブン-イレブンオーナーを知ることができるコンテンツを続々公開しています。<span>&nbsp;</span>
+          </div>
+          <p class="intro__content__btn">詳しくはクリック</p>
+        </a>
+      </div>
+      <ul class="intro__list">
+        <li>
+          <a href="/owner/contents/owner-interview/056.php">
+            <p class="intro__list__tmb">
+              <img src="../owner/assets/img/top/intro-tmb-01.jpg" alt="オーナーインタビュー縁を呼ぶ本気の商売を次代へつなぐ">
+            </p>
+            <div class="intro__list__content">
+              <h3 class="intro__list__title">
+                <span>オーナーインタビュー</span>縁を呼ぶ本気の商売を次代へつなぐ
+              </h3>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="/owner/contents/product-appeal/">
+            <p class="intro__list__tmb">
+              <img src="../owner/assets/img/top/intro-tmb-02.jpg" alt="セブン-イレブンの商品力毎週新商品が続々登場！セブン-イレブンの商品力">
+            </p>
+            <div class="intro__list__content">
+              <h3 class="intro__list__title">
+                <span>セブン-イレブンの商品力</span>毎週新商品が続々登場！セブン-イレブンの商品力
+              </h3>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <p class="intro__btn">
+        <a href="/owner/contents/" class="btn__green_l">もっと読む</a>
+      </p>
+    </div>
+  </section>
+
+  <section class="question">
+    <div class="question__inner inner">
+      <h2 class="heading">よくあるご質問</h2>
+      <div class="question__content">
+        <ul class="question__list">
+          <li>
+            <a href="/owner/faq/#qa_01">オーナーになる条件を教えてください。</a>
+          </li>
+          <li>
+            <a href="/owner/faq/#qa_02">開業資金はどれだけ必要でしょうか？</a>
+          </li>
+          <li>
+            <a href="/owner/faq/#qa_03">契約期間は設定されていますか？</a>
+          </li>
+          <li>
+            <a href="/owner/faq/#qa_04">外国人でも加盟できますか？</a>
+          </li>
+        </ul>
+        <p class="question__content__link">
+          <a href="/owner/faq/" class="btn__arrow">質問一覧へ</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="area" id="area">
+    <div class="inner">
+      <h2 class="heading">地域別情報</h2>
+      <div class="area__list">
+        <a href="/owner/area/market/hokkaido.php">北海道</a>
+        <a href="/owner/area/market/tohoku.php">東北</a>
+        <a href="/owner/area/market/kanto.php">関東</a>
+        <a href="/owner/area/market/koushinetsu.php">甲信越</a>
+        <a href="/owner/area/market/hokuriku.php">北陸</a>
+        <a href="/owner/area/market/tokai.php">東海</a>
+        <a href="/owner/area/market/kansai.php">関西</a>
+        <a href="/owner/area/market/chushikoku.php">中国・四国</a>
+        <a href="/owner/area/market/kyushu.php">九州</a>
+        <a href="/owner/area/market/okinawa.php">沖縄</a>
+      </div>
+    </div>
+  </section>
+
+  <div class="cvr">
+    <div class="cvr__inner inner">
+      <p class="cvr__btn">
+        <a href="/owner/seminar/kensaku/" class="btn__red_l">説明会予約</a>
+      </p>
+      <p class="cvr__btn">
+        <a href="/owner/contact/" class="btn__blue_l">資料請求</a>
+      </p>
+    </div>
+  </div>
+
+  <?php include("${Path}owner/inc/footer.php"); ?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>
+  <script src="<?php print $Path; ?>owner/assets/js/top.js"></script>
+  <script src="<?php print $Path; ?>owner/assets/js/function.js"></script>
+  <?php include("${Path}owner/inc/tag-common-footer.php");
+  ?>
+
+  <script id="tagjs" type="text/javascript">
+    (function () {
+      var tagjs = document.createElement("script");
+      var s = document.getElementsByTagName("script")[0];
+      tagjs.async = true;
+      tagjs.src = "//s.yjtag.jp/tag.js#site=vqAksvX";
+      s.parentNode.insertBefore(tagjs, s);
+    }());
+  </script>
+  <noscript>
+    <iframe src="//b.yjtag.jp/iframe?c=vqAksvX" width="1" height="1" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+  </noscript>
+
+  <!--googleアナリティクス --> 
+  <script type="text/javascript">
+
+  var _gaq = _gaq || []; 
+
+  _gaq.push(['_setAccount', 'UA-16273642-1']);
+
+  _gaq.push(['_setDomainName', '.sej.co.jp']);
+
+  _gaq.push(['_trackPageview']);
+
+  (function() { 
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; 
+
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); 
+
+  })(); 
+
+  </script> 
+
+  <!-- User Insight PCDF Code Start :  -->
+  <script type="text/javascript">
+  var _uic = _uic ||{}; var _uih = _uih ||{};_uih['id'] = 54389;
+  _uih['lg_id'] = '';
+  _uih['fb_id'] = '';
+  _uih['tw_id'] = '';
+  _uih['uigr_1'] = ''; _uih['uigr_2'] = ''; _uih['uigr_3'] = ''; _uih['uigr_4'] = ''; _uih['uigr_5'] = '';
+  _uih['uigr_6'] = ''; _uih['uigr_7'] = ''; _uih['uigr_8'] = ''; _uih['uigr_9'] = ''; _uih['uigr_10'] = '';
+  _uic['uls'] = 1;
+
+  /* DO NOT ALTER BELOW THIS LINE */
+  /* WITH FIRST PARTY COOKIE */
+  (function() {
+  var bi = document.createElement('script');bi.type = 'text/javascript'; bi.async = true;
+  bi.src = '//cs.nakanohito.jp/b3/bi.js';
+  var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(bi, s);
+  })();
+  </script>
+  <!-- User Insight PCDF Code End :  -->
+</body>
+
+</html>
